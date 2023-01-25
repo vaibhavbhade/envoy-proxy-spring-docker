24d9d2225776ca1973c35af73056d088211ab9ab
VaibhavBade <vaibhav.bade@atyeti.com>
2023-01-25 15:58:18 +0530

    intial commit

--- src/main/java/com/atyeti/stresstest/controller/MainController.java
+++ src/main/java/com/atyeti/stresstest/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.atyeti.stresstest.controller;
 
-import jdk.nashorn.internal.runtime.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
