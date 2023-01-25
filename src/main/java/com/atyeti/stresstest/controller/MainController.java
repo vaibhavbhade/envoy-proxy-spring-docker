dd459e039c18ea27c8e37aef9267eb8d78426436
VaibhavBade <vaibhav.bade@atyeti.com>
2023-01-25 15:36:58 +0530

    intial commit

--- src/main/java/com/atyeti/stresstest/controller/MainController.java
+++ src/main/java/com/atyeti/stresstest/controller/MainController.java
@@ -16,7 +16,19 @@ public class MainController {
 
     @GetMapping("/testapi")
     public void testLinux() throws IOException, InterruptedException {
+        /*ProcessBuilder processBuilder = new ProcessBuilder(new String[0]);
+        processBuilder.command("bash", "/c", "sudo get-apt install stress");
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
+        String line;
+        while((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("\nExited with error code : " + exitCode);
+    }*/
         System.out.println("testapi called ................");
     }
 }
